970d14d2d4f4faca91c74a482b3cf4c7e15621f1
Mansei <yarden835@gmail.com>
2022-01-19 15:42:52 +0200

    conflict changes

--- src/cmps/Story.jsx
+++ src/cmps/Story.jsx
@@ -1,31 +1,37 @@
-import {connect} from 'react-redux'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { DynamicCmp } from './dynamicCmps/DynamicCmp';
 
-function _Story(){
-    // map of stories
-
-    return (
-        <section>
-        <h1>Story</h1>
-        </section>
-    )
-
+function _Story({ story, cmpOrder }) {
+	//GET FROM STORE
+	const cmpsOrder = [
+		'status-picker',
+		'member-picker',
+		'date-picker',
+		'priority-picker',
+	];
+	return (
+		<section>
+			<h5>{story.txt}</h5>
+			{cmpsOrder.map((cmp, idx) => {
+				return (
+					<DynamicCmp
+						cmp={cmp}
+						key={idx}
+						onUpdate={(data) => {
+							console.log('Updating: ', cmp, 'with data:', data);
+							// make a copy, update the task
+							// Call action: updateTask(task)
+						}}
+					/>
+				);
+			})}
+		</section>
+	);
 }
 
-
-
-
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
-        // board: state.boardModule.board,
-        // users: state.userModule.users,
-        // loggedInUser: state.userModule.loggedInUser
+        story: state.boardModule.board
     }
 }
-
-const mapDispatchToProps ={
-
-}
-
-
-
-export const Story = connect(mapStateToProps, mapDispatchToProps)(_Story)
