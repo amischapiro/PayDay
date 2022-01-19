0fa33648c18c1db6103714515ebf88f91b1f8adb
Mansei <yarden835@gmail.com>
2022-01-19 13:38:11 +0200

    sidebar

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -1,2 +1,39 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
+import { logout } from '../store/user.action';
 
+function _SideBar({ user, logout }) {
+	return (
+		<section className="side-bar">
+			<div className="actions-top">
+				<button></button>
+				<button></button>
+				<button className="fa bell"></button>
+				<button className="fa inbox"></button>
+				<button className="fa calendar-check"></button>
+			</div>
+			<button className="see-plans">See plans</button>
+			<div className="actions-bottom">
+				<button className="fa puzzle-piece"></button>
+				<button className="fa user-plus"></button>
+				<button className="fa search"></button>
+				<button className="fa question"></button>
+				<button></button>
+			</div>
+		</section>
+	);
+}
+
+function mapStateToProps(state) {
+	return {
+		user: state.userModule.loggedinUser,
+	};
+}
+
+const mapDispatchToProps = {
+	logout,
+};
+
+export const SideBar = connect(mapStateToProps, mapDispatchToProps)(_SideBar)
