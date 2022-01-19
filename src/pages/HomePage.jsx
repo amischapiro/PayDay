b1b14e7dd9f413428dab7f4893d839e4ccb7330d
amischapiro <amischapiro@gmail.com>
2022-01-19 11:55:28 +0200

    home

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Logo from '../../'
+
+
+export function HomePage(){
+
+
+    return(
+        <section>
+            {/* <HomePageHeader /> */}
+            <div>
+                <img src="" alt="" />
+                <h1>ayDay</h1>
+            </div>
+
+
+
+        </section>
+    )
+}
