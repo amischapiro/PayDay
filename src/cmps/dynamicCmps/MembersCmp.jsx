fe0b6e884e910117bfe98005c4cce8cb307c97ee
Mansei <yarden835@gmail.com>
2022-01-19 17:35:32 +0200

    dynamic cmp again

--- src/cmps/dynamicCmps/MembersCmp.jsx
+++ src/cmps/dynamicCmps/MembersCmp.jsx
@@ -4,18 +4,24 @@ export function MembersCmp({ story, onUpdate, boardMembers }) {
 	return (
 		<span
 			onClick={() => {
-				return boardMembers.map((member) => {
-					const nameArr = member.fullname.split(' ');
-					const fName = nameArr[0].split('');
-					const lName = nameArr[1].split('');
-					return (
-						<img
-							onClick={() => onUpdate('ADD_MEMBER', member._id)}
-							src={member.imgUrl}
-							alt={fName[0] + lName[0]}
-						/>
-					);
-				});
+				return (
+					<div className="board-members-list">
+						{boardMembers.map((member) => {
+							const nameArr = member.fullname.split(' ');
+							const fName = nameArr[0].split('');
+							const lName = nameArr[1].split('');
+							return (
+								<img
+									onClick={() =>
+										onUpdate('ADD_MEMBER', member._id)
+									}
+									src={member.imgUrl}
+									alt={fName[0] + lName[0]}
+								/>
+							);
+						})}
+					</div>
+				);
 			}}>
 			{members.map((member) => {
 				const nameArr = member.fullname.split(' ');
