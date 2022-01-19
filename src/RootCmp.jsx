1756b0e4835f8befdc84d9acd2c75b6feba45e14
Mansei <yarden835@gmail.com>
2022-01-19 11:33:25 +0200

    starter

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,11 +1,25 @@
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router';
 
-export function RootCmp() {
-    return (
-        <div className="App">
-            <header className="App-header">
-            </header>
-        </div>
-    );
+import routes from './routes.js';
+
+export class RootCmp extends React.Component {
+	render() {
+		return (
+			<div>
+				<main>
+					<Switch>
+						{routes.map((route) => (
+							<Route
+								key={route.path}
+								exact
+								component={route.component}
+								path={route.path}
+							/>
+						))}
+					</Switch>
+				</main>
+			</div>
+		);
+	}
 }
-
