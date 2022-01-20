9af657555cb3f337f80e174249ace5be85081cb9
amischapiro <amischapiro@gmail.com>
2022-01-20 10:43:47 +0200

    toggle btn

--- src/cmps/BoardList.jsx
+++ src/cmps/BoardList.jsx
@@ -38,7 +38,7 @@ function _BoardList(props) {
 
     return (
         <section className={`boardlist-container ${isBoardListOpen ? 'open':''}`} >
-            <button className='toggle-btn' onClick={()=>onToggleBoardListShown()} >^</button>
+            <button className={`toggle-btn fa-solid ${isBoardListOpen ? 'angleleft':'angleright'} `} onClick={()=>onToggleBoardListShown()} ></button>
             <button className='workspace-toggle' aria-describedby={id} type="button" onClick={handleClick}>
                 <h2>Main workspace <span className={`fa-solid ${open ?'angleup':'angledown'} `} ></span> </h2>
             </button>
