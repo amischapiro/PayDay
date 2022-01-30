d228696770d2b806562f2f7db67180303f9fd151
amischapiro <amischapiro@gmail.com>
2022-01-30 22:11:25 +0200

    logout

--- src/store/user.action.js
+++ src/store/user.action.js
@@ -4,6 +4,7 @@ import { userService } from "../services/user.service.js";
 export function login(credentials) {
     return async (dispatch) => {
         try {
+            
             const user = await userService.login(credentials)
             dispatch({ type: 'SET_USER', user })
             return Promise.resolve()
