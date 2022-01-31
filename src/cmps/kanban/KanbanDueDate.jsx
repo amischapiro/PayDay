e32549f0baf2f4176be952e1d4bec1ab70ac5e3c
Mansei <yarden835@gmail.com>
2022-01-31 15:40:42 +0200

    kanban

--- src/cmps/kanban/KanbanDueDate.jsx
+++ src/cmps/kanban/KanbanDueDate.jsx
@@ -1,5 +1,9 @@
 export function KanbanDueDate({ story }) {
 	const { dueDate } = story.storyData;
+    const dueDateNew = new Date(dueDate);
     
-	return <div></div>;
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const displayDate = `${monthNames[dueDateNew.getMonth()]} ${dueDateNew.getDate()}`;
+
+	return <div className="kanban-dude-date-cmp">{displayDate}</div>;
 }
